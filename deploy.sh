1b47b381009f660391c732f49677dac0ee8342f6
Eric Wilson <e.wilson.1@elsevier.com>
2014-12-30 22:24:54 -0500

    modified deploy script to avoid 403

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp js s3://riskroll/js --recursive --profile eric
-aws s3 cp index.html s3://riskroll/index.html --profile eric
+aws s3 cp js s3://riskroll/js --recursive --acl public-read --profile eric
+aws s3 cp index.html s3://riskroll/index.html --acl public-read --profile eric
